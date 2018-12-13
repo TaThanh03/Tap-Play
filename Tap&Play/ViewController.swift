aa453f56a33e208cfea9ca4a94ee64ed339c5ef3
TA THANH <ta.thanh.03@gmail.com>
2018-12-13 16:25:22 +0100

    Initial Commit

---
+++ Tap&Play/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tap&Play
+//
+//  Created by TA Trung Thanh on 13/12/2018.
+//  Copyright © 2018 TA Trung Thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
