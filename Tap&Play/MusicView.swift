e1445e3aed52c520918e6f26060fbe4df2a1571b
TA THANH <ta.thanh.03@gmail.com>
2018-12-16 03:10:26 +0100

    get songs ok

--- Tap&Play/MusicView.swift
+++ Tap&Play/MusicView.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import MediaPlayer
 
-class MusicView: UIView {
+class MusicView: UIView, MPMediaPickerControllerDelegate {
+    
+    private let myMediaPlayer = MPMusicPlayerController.applicationMusicPlayer
+    private let myMusicPicker = MPMediaPickerController(mediaTypes: MPMediaType.music)
+    private let myMusicQuery = MPMediaQuery.songs()
     
     private let label_status = UILabel()
     private let label_songTitle = UILabel()
     private let label_songNumber = UILabel()
+    private var switch_play = false
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -27,14 +33,12 @@ class MusicView: UIView {
         self.addGestureRecognizer(tapDetect)
         self.addGestureRecognizer(swipeLeft)
         self.addGestureRecognizer(swipeRight)
-        
+        //music
+        myMediaPlayer.setQueue(with: myMusicQuery)
+        myMediaPlayer.repeatMode = .all
         
         label_status.text = ""
-        
-        
-        
-        
-        
+        label_status.textColor = UIColor.white
         
         self.displayInSize(size: UIScreen.main.bounds.size)
     }
@@ -46,19 +50,28 @@ class MusicView: UIView {
     @objc func tapDetection(sender: UITapGestureRecognizer){
         if sender.state == .ended {
             print("double tap")
+            if switch_play == false {
+                myMediaPlayer.play()
+                switch_play = true
+            } else {
+                myMediaPlayer.pause()
+                switch_play = false
+            }
         }
     }
-    
     @objc func swipeDetection(sender: UISwipeGestureRecognizer){
         if sender.direction == .left {
             print("swipe left")
+            myMediaPlayer.skipToNextItem()
         }
         if sender.direction == .right {
             print("swipe right")
+            myMediaPlayer.skipToPreviousItem()
         }
     }
     
     func displayInSize(size: CGSize) {
         
     }
+    
 }
